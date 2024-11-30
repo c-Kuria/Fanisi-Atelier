d22fe3ac72dd2e1c5e528960444a45383840f299
c-Kuria <briankuria@gmail.com>
2024-11-30 14:43:24 +0300

    init

--- fanisiAtelier_project/fanisiAtelier_app/views.py
+++ fanisiAtelier_project/fanisiAtelier_app/views.py
@@ -1,3 +1,61 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
-# Create your views here.
+# Create your views here
+x = datetime.datetime.now()
+date = x.strftime("%x")
+
+def index(request):
+    return render(request, 'fanisi_atelier_app/index.html')
+
+def about(request):
+    return render(request, 'fanisi_atelier_app/about.html')
+
+def services(request):
+    return render(request, 'fanisi_atelier_app/services.html')
+
+def portfolio(request):
+    return render(request, 'fanisi_atelier_app/portfolio.html')
+
+def blog(request):
+    return render(request, 'fanisi_atelier_app/blog.html')
+
+def top_5_interior_design_trends_2024(request):
+    return render(request, 'fanisi_atelier_app/top-5-interior-design-trends-2024.html', {'date': date})
+
+def how_to_choose_the_right_color_palette_for_your_home(request):
+    return render(request, 'fanisi_atelier_app/how-to-choose-the-right-color-palette-for-your-home.html',{'date':date})
+
+def maximazing_small_spaces_design_tips_and_tricks(request):
+    return render(request, 'fanisi_atelier_app/maximazing-small-spaces-design-tips-and-tricks.html',{'date':date})
+
+def the_importance_of_lighting(request):
+    return render(request, 'fanisi_atelier_app/the-importance-of-lighting.html',{'date':date})
+
+from django.core.mail import send_mail, EmailMessage
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def send_email(request):
+    if request.method == 'POST':
+        from_email = request.POST.get('email')
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+
+        try:
+            email = EmailMessage(
+                subject='New Message from Fanisi Atelier',
+                body=f"Name: {name}\n\nMessage: {message}",
+                from_email='Fanisi Atelier <briannkuria@gmail.com>',  # From email
+                to=['briannkuria@gmail.com'],  # To email
+                reply_to=[from_email],  # Reply-To email
+            )
+            email.send(fail_silently=False)
+            return JsonResponse({'success': 'Email sent successfully'}, status=200)
+        except Exception as e:
+            print('Error sending email:', e)
+            return JsonResponse({'error': 'Error sending email'}, status=500)
+
+    return JsonResponse({'error': 'Invalid request method'}, status=405)
--- fanisiAtelier_project/fanisiAtelier_project/settings.py
+++ fanisiAtelier_project/fanisiAtelier_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fanisiAtelier_app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'fanisiAtelier_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +118,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'root_media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'root_static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'briannkuria@gmail.com'
+EMAIL_HOST_PASSWORD = 'bbze japt ysgo kygi'
+
