7f689fcfcb92cd3c5bcc958b116bfca3d3410c51
c-Kuria <briankuria@gmail.com>
2024-12-02 16:34:43 +0300

    init

--- fanisiAtelier_project/fanisiAtelier_project/settings.py
+++ fanisiAtelier_project/fanisiAtelier_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+f=56e62ztpp!46)u%h)alspxa2d1wuigwi@^*j^s-k97_ej%n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.100.254']
 
 
 # Application definition
