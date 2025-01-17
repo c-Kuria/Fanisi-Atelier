069a27165658492f6a87387d569c523cc9ed80ec
c-Kuria <briankuria1812@gmail.com>
2025-01-17 16:08:11 +0300

    updated

--- fanisiAtelier_project/fanisiAtelier_app/views.py
+++ fanisiAtelier_project/fanisiAtelier_app/views.py
@@ -6,39 +6,40 @@ from django.conf import settings
 # Create your views here
 x = datetime.datetime.now()
 date = x.strftime("%x")
+year = x.year
 
 def index(request):
-    return render(request, 'fanisi_atelier_app/index.html')
+    return render(request, 'fanisi_atelier_app/index.html', {'year':year})
 
 def about(request):
-    return render(request, 'fanisi_atelier_app/about.html')
+    return render(request, 'fanisi_atelier_app/about.html', {'year':year})
 
 def services(request):
-    return render(request, 'fanisi_atelier_app/services.html')
+    return render(request, 'fanisi_atelier_app/services.html', {'year':year})
 
 def portfolio(request):
-    return render(request, 'fanisi_atelier_app/portfolio.html')
+    return render(request, 'fanisi_atelier_app/portfolio.html', {'year':year})
 
 def blog(request):
-    return render(request, 'fanisi_atelier_app/blog.html')
+    return render(request, 'fanisi_atelier_app/blog.html', {'year':year})
 
 def top_5_interior_design_trends_2024(request):
-    return render(request, 'fanisi_atelier_app/top-5-interior-design-trends-2024.html', {'date': date})
+    return render(request, 'fanisi_atelier_app/top-5-interior-design-trends-2024.html', {'date': date, 'year':year})
 
 def how_to_choose_the_right_color_palette_for_your_home(request):
-    return render(request, 'fanisi_atelier_app/how-to-choose-the-right-color-palette-for-your-home.html',{'date':date})
+    return render(request, 'fanisi_atelier_app/how-to-choose-the-right-color-palette-for-your-home.html',{'date':date, 'year':year})
 
 def maximazing_small_spaces_design_tips_and_tricks(request):
-    return render(request, 'fanisi_atelier_app/maximazing-small-spaces-design-tips-and-tricks.html',{'date':date})
+    return render(request, 'fanisi_atelier_app/maximazing-small-spaces-design-tips-and-tricks.html',{'date':date, 'year':year})
 
 def the_importance_of_lighting(request):
-    return render(request, 'fanisi_atelier_app/the-importance-of-lighting.html',{'date':date})
+    return render(request, 'fanisi_atelier_app/the-importance-of-lighting.html',{'date':date, 'year':year})
 
 def terms_and_conditions(request):
-    return render(request, 'fanisi_atelier_app/terms-and-conditions.html')
+    return render(request, 'fanisi_atelier_app/terms-and-conditions.html', {'year':year})
 
 def privacy_policy(request):
-    return render(request, 'fanisi_atelier_app/privacy-policy.html')
+    return render(request, 'fanisi_atelier_app/privacy-policy.html', {'year':year})
 
 from django.core.mail import send_mail, EmailMessage
 from django.http import JsonResponse
